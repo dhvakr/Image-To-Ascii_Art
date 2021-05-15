47eb5378f5a60149f551bafc9d99a6fca6334fa0
DIVAKARAN <dhvakr@gmail.com>
2021-05-15 18:59:34 +0530

    Version  4.5 [ Initial Release ]

---
+++ ImageAsciiConverter.java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+/**
+  *  Program to Display an image in ASCII Art Format as Output,
+  *
+  *    1.) Compile a java file "AsciiImageConverter.java"
+  *
+  *    2.) Run the class File in CLI(Command Prompt / Terminal) with image name
+  *            Example = java AsciiImageConverter java.png
+  *                       * (java.png is the image name of ur expecting image)
+  *                       *  Load any image you're interested in (Make sure u are typing correct image name)
+  *
+  *    -> If you expecting to Run this code in your preferred IDE, skip all other instruction and pass the image name directly in " line no 64 "
+  *                       * (i,e) remove fileName[0] and pass your arugument as your image FileName ('java.png") instead of fileName[0]
+  *            Example @Line 64 =====> imageConv.convertImageToAscii("java.png");
+  *
+  *    -> This program also supports single launch source file https://openjdk.java.net/jeps/330, Run the program directly with 
+  *                1.)  java AsciiImageConverter.java java.png
+  *                2.)  You are expected to have java-11 to perform single launch source file execution in terminal
+  *
+  *    ** NOTE :  
+  *                1.) Algorithm Tested with jpg and png Image formats only
+  *                2.) Always run below 150 * 150 dimension height and width images for better viewing
+  *                         (provided some images for testing the code , Check SampleImages/ Folder)
+  *                3.) Keep executing images in source file directory --> (i,e) Outside of SampleImage folder
+  *
+  *    \> Good Luck ☺, Do Leave a star on github if u like the project..
+  *     
+  *  @Github -> https://github.com/dhvakr/Image-To-Ascii_Art
+  *  @Twitter -> https://twitter.com/dhvakr  
+  *           
+  *  @version May-15
+  *  @since 2021
+  *  @author DIVAKARAN
+*/ 
+
+public final class ImageAsciiConverter 
+{
+    private BufferedImage img;
+    private double pixelValue;
+    private PrintWriter prntwrt;
+    private FileWriter filewrt;
+
+    public ImageAsciiConverter() throws IOException 
+    {
+        prntwrt = new PrintWriter(filewrt = new FileWriter("asciiart.txt", true));
+    }
+    
+    /**
+     * 
+     * @param fileName
+     *        Takes image name while excuting via terminal
+     * @exception IOException 
+     *            if file is not found or readable
+     */
+    public static void main(final String... fileName)
+    {
+        try 
+        {
+          ImageAsciiConverter imageConv = new ImageAsciiConverter();
+          imageConv.convertImageToAscii("java.png");
+
+          FileReader fr = new FileReader("asciiart.txt");
+          int i;
+          while ((i = fr.read()) != -1) 
+          {
+              System.out.print((char) i);
+          }
+          PrintWriter writer = new PrintWriter("asciiart.txt");
+          writer.print("");
+          writer.close();
+          fr.close();
+        }
+        catch(final ArrayIndexOutOfBoundsException nullValue)
+        {
+          System.out.println("\nPlease enter imagename after -> [ java ImageAsciiConverter [ImageName] ]");
+          System.out.println("\t--------- OR ---------");
+          System.out.println("Try Run -> \' java ImageAsciiConverter java.png \'");
+          System.out.println();
+        }
+        catch(final IOException notFound)
+        {
+            System.err.print("Error !! , check the IMAGE NAME you Entered  ");
+            System.out.print("[ OR ]  Make Sure you kept images in source folder");
+        }
+    }
+    /**
+     * Method to Take an image and Convert them into Ascii format
+     * 
+     * @param imageName     Takes Image to convert into asciiart
+     * @throws IOException  
+     */
+    public void convertImageToAscii(final String imageName) throws IOException 
+    {
+        img = ImageIO.read(new File(imageName));   
+
+        for (int i = 0; i < img.getHeight(); i++) 
+        {
+            for (int j = 0; j < img.getWidth(); j++) 
+            {
+                Color pixelcolor = new Color(img.getRGB(j, i));
+                pixelValue = (((pixelcolor.getRed() * 0.30) + (pixelcolor.getBlue() * 0.59) + (pixelcolor.getGreen() * 0.11)));
+                print(colorToChar(pixelValue));  // pass pixel value to print
+            }
+            prntwrt.println("");
+            prntwrt.flush();
+            filewrt.flush(); 
+        }
+    }
+    /**
+     * This Method return the ASCII art to print method
+     * 
+     * @param pixval
+     * @return a character to print 
+     */
+    public char colorToChar(final double pixval) 
+    {
+        if (pixval >= 240) 
+          return ' ';
+        else if (pixval >= 210) 
+          return '.';
+        else if (pixval >= 190) 
+          return '*';
+        else if (pixval >= 170) 
+          return '+';
+        else if (pixval >= 120) 
+          return '^';
+        else if (pixval >= 110) 
+          return '$';
+        else if (pixval >= 80) 
+          return '4';
+        else if (pixval >= 60) 
+          return '#';
+        else 
+          return ' '; 
+    }
+    /**
+     * Prints the ascii characters
+     * 
+     * @param asciiArt
+     * @throws IOException
+     */
+    public void print(char asciiArt) throws IOException 
+    {
+        prntwrt.print(asciiArt);
+        prntwrt.flush();
+        filewrt.flush();    
+    }   
+}
+    
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# Image-To-Ascii_Art
-Program to Display an image in ASCII art format in java
+# Image-To-Ascii_Art in Java
+[![License: The Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+[![Twitter Follow](https://img.shields.io/twitter/follow/dhvakr?label=Twitter&style=social)](https://twitter.com/dhvakr)
+[![GitHub followers](https://img.shields.io/github/followers/dhvakr?label=Follow&style=social)](https://github.com/dhvakr?tab=followers)
+
+ **This contain code to Convert an Image to Ascii using java, Refer below steps to get started** 
+## Tested Image Formats
+**PNG | JPG**
+
+
+### `STEP 1 :`
+      Compile a java file "AsciiImageConverter.java"
+## **Figure 1**
+<p align = "center">
+<img src = "Demo-Images/Compile.png" height = "" width = "840"  alt = "imageView Demo"/>
+</p>
+
+### `STEP 2 :`
+   Run the class File in CLI (Command Prompt / Terminal) with image name
+  ```yaml
+    Example : java AsciiImageConverter java.png
+
+    (java.png is the image name of ur expecting image)
+    Refer : Figure 2
+```
+* Load any image you're interested in (Make sure u are typing correct image name)
+
+  
+## **Figure 2**
+<p align = "center">
+<img src = "Demo-Images/Run.png" height = "" width = "840"  alt = "imageView Demo"/>
+</p>
+
+> If you expecting to Run this code in your preferred IDE, Instead of CLI, skip all other instruction and pass the image name directly in `line no 64` at `ImageAsciiConverter.java` file
+  
+   (i,e) at line no : 64 remove fileName[0] and pass argument as your image name ('java.png") instead of fileName[0] 
+```yaml
+  Refer: Figure 3
+```
+> @ Line 64 => imageConv.convertImageToAscii(`"java.png"`);
+
+## **Figure 3**
+<p align = "left">
+<img src = "Demo-Images/IDE.png" height = "" width = "750"  alt = "imageView Demo"/>
+</p>
+
+**Excepted Change To Run via IDE**
+
+<p align = "right">
+<img src = "Demo-Images/IDEChange_Example.png" height = "" width = "750"  alt = "imageView Demo"/>
+</p>
+
+This code also supports single launch source file, Run this code with single command `java AsciiImageConverter.java java.png`  in terminal
+
+  > Refer : [JEP330](https://openjdk.java.net/jeps/330) for more information.
+
+  You are expected to have java-11 to perform single launch source file execution in terminal
+
+### TO-CONTRIBUTE :
+
+```yaml
+Clone Repo :
+        https://github.com/dhvakr/Image-To-Ascii_Art
+```  
+Found any bug !? Please create an issue, before making Pull Request
+
+---
+### `LICENSE -> ` 
+
+Distributed under the The Unlicense. See [LICENSE](https://github.com/dhvakr/Image-To-Ascii_Art/blob/main/LICENSE) for more information.
+
+---
+### REMEMBER :
+*   Algorithm Tested with jpg and png Image formats only
+*   Try to run images below 150 * 150 dimension height and width for better viewing 
+    (provided some images for testing this code , Check `SampleImages/` Folder)
+*   Keep executing images in source file directory (i,e) Outside of Sample Image folder
+
+### Contact :
+
+``` Twitter : ``` [@dhvakr](https://twitter.com/dhvakr)
+
+*Feedbacks are precious, please make some time to give your feedback, Thank u☺*
